ce5478444dc4abc24d11839faee5d03650a5bdb0
Patryk Ptak <ptaszqq@gmail.com>
2022-03-19 08:30:28 +0000

    sidebar

---
+++ components/playerLayout.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Box } from '@chakra-ui/layout';
+import Sidebar from './sidebar';
+
+const PlayerLayout = ({ children }) => {
+  return (
+    <Box width="100vw" height="100vh">
+      <Box position="absolute" top="0" width="250px" left="0">
+        <Sidebar />
+      </Box>
+      <Box marginLeft="250px" marginBottom="100px">
+        {children}
+      </Box>
+      <Box
+        position="absolute"
+        left="0"
+        bottom="0"
+        height="100px"
+        background="gray.800"
+        width="100%"
+      >
+        Player
+      </Box>
+    </Box>
+  );
+};
+
+export default PlayerLayout;
